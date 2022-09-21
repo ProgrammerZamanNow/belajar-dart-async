a50297216680f70bde2283d653edd5ee9e73f837
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-21 14:51:58 +0700

    setUp Function

---
+++ test/setup_test.dart
@@ -0,0 +1,25 @@
+import 'package:test/test.dart';
+
+void main(){
+
+  var data = "Eko";
+
+  setUp((){
+    data = "Eko";
+  });
+
+  group("Test String", (){
+
+    test("String first", (){
+      data = "$data Kurniawan";
+      expect(data, equals("Eko Kurniawan"));
+    });
+
+    test("String second", (){
+      data = "$data Khannedy";
+      expect(data, equals("Eko Khannedy"));
+    });
+
+  });
+
+}
