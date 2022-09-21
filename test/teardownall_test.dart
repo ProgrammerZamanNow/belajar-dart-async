3f89666f98b14e595365350ca41dbf96ee07c5e9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-21 15:18:55 +0700

    teardownall function

---
+++ test/teardownall_test.dart
@@ -0,0 +1,38 @@
+import 'package:test/test.dart';
+
+void main(){
+
+  var data = "Eko";
+
+  setUpAll((){
+    print("Start unit test");
+  });
+
+  setUp((){
+    data = "Eko";
+    print(data);
+  });
+
+  tearDown((){
+    print(data);
+  });
+
+  tearDownAll((){
+    print("End unit test");
+  });
+
+  group("Test String", (){
+
+    test("String first", (){
+      data = "$data Kurniawan";
+      expect(data, equals("Eko Kurniawan"));
+    });
+
+    test("String second", (){
+      data = "$data Khannedy";
+      expect(data, equals("Eko Khannedy"));
+    });
+
+  });
+
+}
