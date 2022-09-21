ae025dbfb6d692867191e5104bd318a2209794a6
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-09-21 16:17:51 +0700

    Skip Test

---
+++ test/skip_parameter_test.dart
@@ -0,0 +1,16 @@
+
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+int sum(int a, int b) => a + b;
+
+void main() {
+  group("Test sum()", () {
+    test("positive", (){
+      expect(sum(1, 2), equals(3));
+    });
+    test("negative", (){
+      expect(sum(10, -5), equals(5));
+    }, skip: "This test function still broken");
+  });
+}
---
+++ test/skip_test.dart
@@ -0,0 +1,18 @@
+
+@Skip("This unit test still broken")
+
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+int sum(int a, int b) => a + b;
+
+void main() {
+  group("Test sum()", () {
+    test("positive", (){
+      expect(sum(1, 2), equals(3));
+    });
+    test("negative", (){
+      expect(sum(10, -5), equals(5));
+    });
+  });
+}
