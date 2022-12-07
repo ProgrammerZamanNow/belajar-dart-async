0222c7624eeee03b70eddd0c6d0812518b5ec378
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-12-07 09:57:12 +0700

    Stream

--- bin/stream.dart
+++ bin/stream.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 Stream<String> stream() {
   return Stream.periodic(Duration(seconds: 2), (i) {
     if (i % 2 == 0) {
@@ -8,7 +10,22 @@ Stream<String> stream() {
   });
 }
 
-void main(){
-  stream();
+void main() {
+  Stream<String> flow = stream();
+  StreamSubscription<String> listen = flow.listen((event) {
+    print(event); // tergantikan oleh onDone
+  });
+  listen.onData((data) {
+    print("Stream Subscription : $data");
+  });
+  listen.onDone(() {
+    print("Stream Subscription Done");
+  });
+
+  // error, tidak bisa di listen 2x
+  // StreamSubscription<String> listen2 = flow.listen((event) {
+  //   print(event);
+  // });
+
   print("Done");
 }
